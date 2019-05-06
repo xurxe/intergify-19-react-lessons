fa950b45feac9080da94a3974c488083e222e977
Xurxe <xurxe.garcia@integrify.io>
2019-05-06 15:25:28 +0300

    Added notes from 2019.05.06

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/* import App from './App';
-import './index.css';
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement); */
 
 const root = document.getElementById("root");
-
-/* 
-const name = 'Xurxe'
-const year = new Date().getFullYear();
-const title = <h1>Hello {name}! ({year})</h1>
-
-const header = (
-    <header>
-        <h1>
-            {title}
-        </h1>
-    </header>
-) */
 
 const Header = (props) => {
     const {year, children} = props;
@@ -31,9 +14,7 @@ const Header = (props) => {
             {children}
         </header>
     );
-}
-
-/* ReactDOM.render(header, root); */
+};
 
 const Div = (props) => {
     return (
@@ -52,79 +33,82 @@ const Main = () => {
             <Div>Div 2</Div>
             <Div>Div 3</Div>
         </main>
-    )
-}
+    );
+};
 
 const Greeting = (props) => {
     const {name} = props;
     return (
         <h3>Welcome, {name}!</h3>
-    )
-}
-
+    );
+};
 class Site extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            count: 0,
-            isLoggedIn: false,
-            name: 'Xurxe',
-            todos: ['Todo 1', 'Todo 2', 'Todo 3'],
-            todo: '',
+    state = {
+        count: 0,
+        isLoggedIn: false,
+        name: 'Xurxe',
+        todos: ['Todo 1', 'Todo 2', 'Todo 3'],
+        todo: '',
+        feedback: '',
+    };
+
+    handleAdd = () => {
+        this.setState((prevState => ({count: prevState.count + 1})));
+    };
+
+    handleSubtract = () => {
+        this.setState((prevState => ({count: prevState.count - 1})));
+    };
+
+    handleLogIn = () => {
+        this.setState({isLoggedIn: !this.state.isLoggedIn});
+    };
+
+    handleInput = (event) => {
+        this.setState({todo: event.target.value});
+    };
+
+    handleDelete = (index) => {
+        const todos = [...this.state.todos];
+        todos.splice(index, 1);
+        this.setState({todos: todos});
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (!this.state.todo) {
+            this.setState({feedback: 'This value cannot be empty.'});
         }
-        this.handleAdd = this.handleAdd.bind(this);
-        this.handleSubtract = this.handleSubtract.bind(this);
-        this.handleLogIn = this.handleLogIn.bind(this);
-        this.handleInput = this.handleInput.bind(this);
 
-    }
-
-/* 
-    handleAdd () {
-        this.setState({count: this.state.count + 1})
-    }
-
-    handleSubtract () {
-        this.setState({count: this.state.count - 1})
-    }
-    
-*/
-
-    handleAdd () {
-        this.setState((prevState => ({count: prevState.count + 1})))
-    }
-
-    handleSubtract () {
-        this.setState((prevState => ({count: prevState.count - 1})))
-    }
-
-    handleLogIn() {
-        this.setState({
-            isLoggedIn: !this.state.isLoggedIn
-        })
-    }
-
-    handleInput(e) {
-        this.setState({
-            todo: e.target.value
-        })
-    }
+        else if (this.state.todos.indexOf(this.state.todo) > -1) {
+            this.setState({feedback: 'This value must be unique.'});
+        }
+        
+        else {
+            this.setState((prevState) => ({
+                todos: [this.state.todo, ...prevState.todos]
+            }))
+        };
+    };
 
     render() {
         const {count, isLoggedIn, todos} = this.state;
         return (
             <div>
-                <Header
-                year={new Date().getFullYear()}>
+                <Header year={new Date().getFullYear()}>
                     I can add child props here.
                 </Header>
+
                 <Main></Main>
+
                 <h2>
                     Count: {count}
                 </h2>
+
                 <button onClick={this.handleAdd}>
                     +1
                 </button>
+
                 <button onClick={this.handleSubtract}>
                     -1
                 </button>
@@ -132,42 +116,54 @@ class Site extends React.Component {
                 <p>
                     Please {isLoggedIn ? 'log out' : 'log in'}:
                 </p>
+
                 <button onClick={this.handleLogIn}>
                     {isLoggedIn ? 'Log out' : 'Log in'}
                 </button>
+
                 {
                     isLoggedIn
                     &&
                     <Greeting name={this.state.name}></Greeting>
                 }
 
-                <p>Add a task to your to-do list:</p>
+                <p>
+                    Add a task to your to-do list:
+                </p>
 
-                <input
-                type='text'
-                value={this.state.todo}
-                placeholder='Task'
-                onChange={(e) => this.handleInput}
-                ></input>
-                <ul>
-                    {todos.map((todo) => {
-                        return <li key={todo}>{todo}</li>
-                    })}
-                </ul>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                    type='text'
+                    value={this.state.todo}
+                    placeholder='Task'
+                    onChange={(event)=> this.handleInput(event)}
+                    ></input>
 
-                
+                    <p>
+                        {this.state.feedback}
+                    </p>
 
+                    <ul>
+                        {todos.map((todo, index) => {
+                            return (
+                                <li key={todo+index}>
+                                    {todo}
+                                    <button onClick = {(index) => this.handleDelete(index)}>
+                                        Delete
+                                    </button>
+                                </li>
+                            )
+                        })}
+                    </ul>
+
+                    <input
+                    type='submit'
+                    ></input>
+                </form>
             </div>
         )
     }
 }
-
-/* ReactDOM.render(
-    <Header
-        name="Xurxe"
-        year={new Date().getFullYear()}
-    >I can add child props here.</Header>, root
-) */
 
 ReactDOM.render(
     <Site></Site>, root
